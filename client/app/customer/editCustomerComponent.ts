1fb86d86156d2a4bed758a720f973a5f5577f28a
Jonny Cavell <jonny.cavell@digital.homeoffice.gov.uk>
2017-10-20 17:18:09 +0100

    Default address values to undefined / United Kingdom / active

--- client/app/customer/editCustomerComponent.ts
+++ client/app/customer/editCustomerComponent.ts
@@ -13,6 +13,7 @@ import _ from "lodash";
 import {Address} from "../shared/address/address.model";
 import {AddressService} from "../shared/address/address.service";
 import {CustomerRecord} from "../shared/customer/customerRecord.model";
+import {isNullOrUndefined} from "util";
 
 
 @Component({
@@ -43,18 +44,8 @@ export class EditCustomerComponent implements OnInit {
     this.getCompanies();
     this.company = _.clone(this.customerRecord.company);
     this.customer = _.clone(this.customerRecord.customer);
-
-    // if (this.invoiceAddress == null) {
-    //   this.invoiceAddress = new Address(null, null, null, null, null, null, null, null, null, null, null, null);
-    // } else {
-      this.invoiceAddress = _.clone(this.customerRecord.invoiceAddress);
-   // }
-
-    // if (this.deliveryAddress == null) {
-    //   this.deliveryAddress = new Address(null, null, null, null, null, null, null, null, null, null, null, null);
-    // } else {
-      this.deliveryAddress = _.clone(this.customerRecord.deliveryAddress);
-   // }
+    this.invoiceAddress = isNullOrUndefined(this.customerRecord.invoiceAddress) ? new Address() : _.clone(this.customerRecord.invoiceAddress);
+    this.deliveryAddress = isNullOrUndefined(this.customerRecord.deliveryAddress) ? new Address() : _.clone(this.customerRecord.deliveryAddress);
   }
 
   search = (text$: Observable<string>) =>
@@ -70,26 +61,27 @@ export class EditCustomerComponent implements OnInit {
     this.modalRef = this.modalService.open(content, {size: 'lg'});
   }
 
-
-  upsertInvoiceAddressPromise(): Observable<Address>  {
-    return _.isEqual(this.customerRecord.invoiceAddress, this.invoiceAddress) ?
+  upsertInvoiceAddress(): Observable<Address>  {
+    return _.isEqual(this.customerRecord.invoiceAddress, this.invoiceAddress ) ||
+            isNullOrUndefined(this.invoiceAddress.name) ?
       Observable.of(this.invoiceAddress) :
       this.addressService.upsertAddress(this.invoiceAddress);
   }
 
-  upsertDeliveryAddressPromise(): Observable<Address> {
-    return _.isEqual(this.customerRecord.deliveryAddress, this.deliveryAddress) ?
+  upsertDeliveryAddress(): Observable<Address> {
+    return _.isEqual(this.customerRecord.deliveryAddress, this.deliveryAddress) ||
+    isNullOrUndefined(this.deliveryAddress.name) ?
       Observable.of(this.deliveryAddress) :
       this.addressService.upsertAddress(this.deliveryAddress);
   }
 
-  updateCompanyPromise(): Observable<Company> {
+  updateCompany(): Observable<Company> {
     return _.isEqual(this.customerRecord.company, this.company) ?
       Observable.of(this.company) :
       this.companyService.editCompany(this.company);
   }
 
-  updateCustomerPromise(): Observable<Customer> {
+  updateCustomer(): Observable<Customer> {
     return _.isEqual(this.customerRecord.customer, this.customer) &&
     _.isEqual(this.customerRecord.invoiceAddress, this.invoiceAddress) &&
     _.isEqual(this.customerRecord.deliveryAddress, this.deliveryAddress) &&
@@ -100,19 +92,19 @@ export class EditCustomerComponent implements OnInit {
 
 
   updateAll() {
-    this.upsertInvoiceAddressPromise().flatMap((a) => {
+    this.upsertInvoiceAddress().flatMap(a => {
       this.customer.invoiceAddressId = a.id;
-      return this.upsertDeliveryAddressPromise();
+      return this.upsertDeliveryAddress();
     }).
-    flatMap((a) => {
+    flatMap(a => {
       this.customer.deliveryAddressId = a.id;
-      return this.updateCompanyPromise();
+      return this.updateCompany();
     }).
-    flatMap((c) => {
+    flatMap(c => {
       this.customer.companyId = c.id;
       this.customerRecord.company = _.clone(c);
       console.log('Updated company ' + JSON.stringify(c));
-      return this.updateCustomerPromise();
+      return this.updateCustomer();
     }).subscribe(
       customer => {
         this.customerRecord.customer = customer;
--- client/app/shared/address/address.model.ts
+++ client/app/shared/address/address.model.ts
@@ -1,15 +1,15 @@
 export class Address {
-  constructor(public id: number,
-              public name: string,
-              public company: string,
-              public fao: string,
-              public line1: string,
-              public line2: string,
-              public line3: string,
-              public townCity: string,
-              public county: string,
-              public postcode: string,
-              public country: string,
-              public active: boolean) {
+  constructor(public id: number = undefined,
+              public name: string = undefined,
+              public company: string = undefined,
+              public fao: string = undefined,
+              public line1: string = undefined,
+              public line2: string = undefined,
+              public line3: string = undefined,
+              public townCity: string = undefined,
+              public county: string = undefined,
+              public postcode: string = undefined,
+              public country: string = 'United Kingdom',
+              public active: boolean = true) {
   }
 }
