9d0dc5faf1d7a1e9a6af8b86eaae85c2b8ace54b
Jonny Cavell <jonny.cavell@digital.homeoffice.gov.uk>
2017-10-24 15:29:25 +0100

    Edit Customer - Remove isEdit, loading divs etc.

--- client/app/customer/customer.component.ts
+++ client/app/customer/customer.component.ts
@@ -3,7 +3,6 @@ import {Http} from "@angular/http";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ToastComponent} from "../shared/toast/toast.component";
 import {CustomerService} from "../shared/customer/customer.service";
-import {Customer} from "../shared/customer/customer.model";
 import {CompanyService} from "../shared/company/company.service";
 import {UserService} from "../shared/user/user.service";
 import {CustomerRecord, IAlert} from "../shared/customer/customerRecord.model";
@@ -63,9 +62,6 @@ export class CustomerComponent implements OnInit {
         this.customers = customerRecords;
       });
 
-    // this.getCustomers();
-    this.getCompanies();
-
     this.addCustomerForm = this.formBuilder.group({
       name: this.name,
       email: this.email,
@@ -114,65 +110,11 @@ export class CustomerComponent implements OnInit {
   }
 
   newCustomerRecord(){
-    return new CustomerRecord()
+    return new CustomerRecord();
   }
 
   customerCreated(cr: CustomerRecord) {
     this.customers.push(cr);
-  }
-  getCompanies() {
-    this.companyService.getCompanies().subscribe(
-      data => {
-        this.companies = data;
-        // this.companies.unshift({'id': '', 'name': 'company'});
-      },
-      error => console.log(error),
-      () => this.isLoading = false
-    );
-  }
-
-  addCustomer() {
-    const customer = new Customer(
-      0,
-      this.addCustomerForm.value.name,
-      '',
-      this.addCustomerForm.value.email,
-      this.addCustomerForm.value.directPhone,
-      this.addCustomerForm.value.mobilePhone,
-      this.addCustomerForm.value.source,
-      this.addCustomerForm.value.position,
-      this.addCustomerForm.value.isMainContact,
-      this.addCustomerForm.value.twitter,
-      this.addCustomerForm.value.facebook,
-      this.addCustomerForm.value.linkedIn,
-      this.addCustomerForm.value.skype,
-      parseInt(this.addCustomerForm.value.companyId, 10),
-      null, // repId
-      null, // invoiceaddressId
-      null, // deliveryAddressId
-      true // active
-    );
-    this.customerService.addCustomer(customer).subscribe(
-      newCustomer => {
-        this.customers.push(newCustomer);
-        this.addCustomerForm.reset();
-        this.toast.setMessage('item added successfully.', 'success');
-      },
-      error => this.toast.setMessage('Error inserting customer: ' + JSON.stringify(error).substr(0, 200), 'danger')
-    );
-  }
-
-  enableEditing(customer) {
-    this.isEditing = true;
-    this.customer = customer;
-  }
-
-  cancelEditing() {
-    this.isEditing = false;
-    this.customer = undefined;
-    this.toast.setMessage('item editing cancelled.', 'warning');
-    // reload the customers to reset the editing
-    this.getCustomers();
   }
 
   deleteCustomer(customer) {
